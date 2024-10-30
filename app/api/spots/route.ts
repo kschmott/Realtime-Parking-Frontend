3da8d70e5134545ee7b164320b6b6b503a08dfc6
kschmott <kschmott@umich.edu>
2024-10-30 16:32:43 -0400

    updates

--- app/api/spots/route.ts
+++ app/api/spots/route.ts
@@ -1,6 +1,6 @@
 export async function POST(req: Request) {
   const body = await req.json();
-  console.log(body);
+  console.log(JSON.stringify(body, null, 2));
   return Response.json({
     message: "Faizan is the best",
     receivedBody: body,
